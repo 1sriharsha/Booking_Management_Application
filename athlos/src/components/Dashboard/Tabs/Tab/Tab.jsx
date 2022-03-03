6d5793ae6c0a79641bba62edff1136739d74e009
Jacob Day <day6@iu.edu>
2022-03-03 17:51:55 -0500

    simplified Tab component

--- athlos/src/components/Dashboard/Tabs/Tab/Tab.jsx
+++ athlos/src/components/Dashboard/Tabs/Tab/Tab.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Tab.css";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Tab extends Component {
@@ -12,24 +11,25 @@ class Tab extends Component {
     const { tabLabel, onClick } = this.props;
     onClick(tabLabel);
   };
+
   render() {
     const {
       onClick,
-      props: { activeTab, tabLabel },
+      props: { activeTab, tabLabel, icon },
     } = this;
 
     let className = "sideTabLink";
 
-    if (this.props.activeTab === this.props.tabLabel) {
+    if (activeTab === tabLabel) {
       className += " activeTabLink";
     }
 
     return (
       <button className={className} onClick={onClick}>
         <i>
-          <FontAwesomeIcon icon={this.props.icon} />
+          <FontAwesomeIcon icon={icon} />
         </i>
-        {this.props.tabLabel}
+        {tabLabel}
       </button>
     );
   }
