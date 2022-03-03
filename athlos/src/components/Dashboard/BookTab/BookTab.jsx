924818d165bec6f1b4588c474c147f2f268ce0ed
Jacob Day <day6@iu.edu>
2022-03-03 09:49:48 -0500

    added BookTab component

---
+++ athlos/src/components/Dashboard/BookTab/BookTab.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "./BookTab.module.css";
+
+class BookTab extends Component {
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default BookTab;
--- athlos/src/components/index.js
+++ athlos/src/components/index.js
@@ -2,7 +2,8 @@
 export { default as Home } from "./Home/Home";
 export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as RegistrationModal } from "./RegistrationModal/RegistrationModal";
-export { default as Sidebar } from "./Sidebar/Sidebar";
-export { default as UserTabs } from "./Sidebar/UserTabs/UserTabs";
-export { default as ManagementTabs } from "./Sidebar/ManagementTabs/ManagementTabs";
+export { default as Sidebar } from "./Dashboard/Sidebar/Sidebar";
+export { default as UserTabs } from "./Dashboard/Sidebar/UserTabs/UserTabs";
+export { default as ManagementTabs } from "./Dashboard/Sidebar/ManagementTabs/ManagementTabs";
 export { default as NavProfile } from "./NavProfile/NavProfile";
+export { default as BookTab } from "./Dashboard/BookTab/BookTab";
