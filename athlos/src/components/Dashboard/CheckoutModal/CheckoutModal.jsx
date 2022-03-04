aadbcb51422a11652f035c4eafe7e9b4d9d4ece2
Jacob Day <day6@iu.edu>
2022-03-04 18:12:26 -0500

    added CheckoutModal component

---
+++ athlos/src/components/Dashboard/CheckoutModal/CheckoutModal.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import styles from "./CheckoutModal.module.css";
+import "./CheckoutModal.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class CheckoutModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default CheckoutModal;
