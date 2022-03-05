dc3d94e61e0b846afdb21aeaeb90cfe598b91987
vaibhav201297 <vaibhav.vishwanath2097@gmail.com>
2022-03-05 15:38:52 -0500

    Adding project API  with gitignore

---
+++ API/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+const bodyParser = require('body-parser');  
+const asyncHandler = require('express-async-handler')
+
+
+
+router.post('/register', asyncHandler(async(req, res) => {
+    let resp = await profileServiceApi.post(`/createProfile`, req.body)
+    res.status(resp.status).send(resp.data)
+  }))
+
+module.exports = router;
