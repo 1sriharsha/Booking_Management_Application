5451ca2081a753bcf77a6ead935cb1399e40e5ee
vaibhav201297 <vaibhav.vishwanath2097@gmail.com>
2022-03-07 16:17:26 -0500

    Heroku and MongoDB CLoud update

--- models/User.js
+++ models/User.js
@@ -1,14 +1,14 @@
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+
+const UserSchema = new mongoose.Schema({
+	userId:{type:String,required:true},
     firstName: {type: String, required: true},
 	lastName: {type: String, required: true},
 	email: {type: String, required: true},
 	password: {type: String, required: true},
-    profileType:String
+	profileType:{type:String,required:true,default:"Manual"}
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model('Users', UserSchema,'users');
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
-const bodyParser = require('body-parser');  
-const asyncHandler = require('express-async-handler')
+const User = require('../models/User');
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+const cors = require('cors')
 
 
-router.get('/',asyncHandler(async(req, res) => {
+
+router.get('/',(req, res) => {
     res.send('Hello')
-  }))
+  })
+  
+router.post('/api',function(req, res, next){
+  
+  console.log(req.body)
+  
+  var user = {
+    userId:new mongoose.Types.ObjectId().toString(),
+    firstName:req.body.firstName,
+    lastName:req.body.lastName,
+    email:req.body.email,
+    password:req.body.password,
+    profileType:"Manual"
+  }
+  //cclonsole.log(user)
+    User.create(user).then(function(users){
+        res.json(users);
+    }).catch(next);
+});
 
-router.post('/register', asyncHandler(async(req, res) => {
-    let resp = await profileServiceApi.post(`/createProfile`, req.body)
-    res.status(resp.status).send(resp.data)
-  }))
 
 module.exports = router;
