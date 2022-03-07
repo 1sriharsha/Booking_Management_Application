f35dc8e26ebe6586c2312fbd5ca5c883a743541c
Jacob Day <day6@iu.edu>
2022-03-06 21:01:09 -0500

    removed unused css

--- athlos/src/index.js
+++ athlos/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./normalize.css";
-import "./styles.css";
 import reportWebVitals from "./reportWebVitals";
 
 // FontAwesome Icons
