07ecd65b509ebe1976cf7c3d3eca490b04586cfc
Jacob Day <day6@iu.edu>
2022-03-08 13:48:37 -0500

    added TimeSlot component

---
+++ athlos/src/components/Dashboard/BookCard/CheckoutModal/TimeSlot/TimeSlot.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./TimeSlot.module.css";
+
+class TimeSlot extends Component {
+  render() {
+    const {
+      props: {},
+    } = this;
+
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default TimeSlot;
--- athlos/src/components/index.js
+++ athlos/src/components/index.js
@@ -10,3 +10,4 @@ export { default as Tab } from "./Dashboard/Sidebar/Tab/Tab";
 export { default as BookCard } from "./Dashboard/BookCard/BookCard";
 export { default as CheckoutModal } from "./Dashboard/BookCard/CheckoutModal/CheckoutModal";
 export { default as GoogleLoginButton } from "./RegistrationModal/GoogleLoginButton/GoogleLoginButton";
+export { default as TimeSlot } from "./Dashboard/BookCard//CheckoutModal/TimeSlot/TimeSlot";
