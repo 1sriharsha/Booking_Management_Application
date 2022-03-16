e3f9cf0b9c0e9bc21deac09dd9b928f1b36065ef
Jacob Day <day6@iu.edu>
2022-03-16 09:11:54 -0500

    added Counter component

---
+++ src/components/Dashboard/BookCard/CheckoutModal/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Counter.css";
+class Counter extends Component {
+  render() {
+    const {
+      props: {},
+    } = this;
+
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default Counter;
--- src/components/index.js
+++ src/components/index.js
@@ -11,3 +11,4 @@ export { default as BookCard } from "./Dashboard/BookCard/BookCard";
 export { default as CheckoutModal } from "./Dashboard/BookCard/CheckoutModal/CheckoutModal";
 export { default as GoogleLoginButton } from "./RegistrationModal/GoogleLoginButton/GoogleLoginButton";
 export { default as TimeSlot } from "./Dashboard/BookCard//CheckoutModal/TimeSlot/TimeSlot";
+export { default as Counter } from "./Dashboard/BookCard/CheckoutModal/Counter/Counter";
