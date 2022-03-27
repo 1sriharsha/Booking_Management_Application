c51323211495a7623e1ebbebf4098914b271633b
Jacob Day <day6@iu.edu>
2022-03-26 20:28:59 -0400

    close search options on selection

--- src/components/Dashboard/Searchbar/Searchbar.jsx
+++ src/components/Dashboard/Searchbar/Searchbar.jsx
@@ -26,7 +26,7 @@ class Searchbar extends Component {
   };
 
   setSearchValue(value) {
-    this.setState({ searchValue: value });
+    this.setState({ searchValue: value, showOptions: false });
   }
 
   render() {
