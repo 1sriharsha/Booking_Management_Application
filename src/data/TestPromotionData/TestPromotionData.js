a5ec1c28c94ace9c837b14e2b52eb065adf8ed84
Jacob Day <day6@iu.edu>
2022-03-31 15:58:20 -0400

    added TestPromotionData.js

---
+++ src/data/TestPromotionData/TestPromotionData.js
@@ -0,0 +1,22 @@
+const TestPromotionData = [
+  {
+    id: 1,
+    promotionName: "10% Off Equipment Rentals",
+    promotionCode: "ATHLOS10",
+    promotionStart: "2022-04-01",
+    promotionEnd: "2022-04-30",
+    promotionPercentage: 0.1,
+    promotionInfo: "Enjoy 10% off equipment rentals for the month of April.",
+  },
+  {
+    id: 2,
+    promotionName: "5% Off",
+    promotionCode: "ATHL05",
+    promotionStart: "2022-01-31",
+    promotionEnd: "2022-12-31",
+    promotionPercentage: 0.05,
+    promotionInfo: "Enjoy 5% off your next reservation.",
+  },
+];
+
+export default TestPromotionData;
