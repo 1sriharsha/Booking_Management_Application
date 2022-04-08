3315c523351941ea6d41a61292671670e77e08f4
Jacob Day <day6@iu.edu>
2022-04-08 17:02:01 -0400

    updated intime & outtime

--- models/Booking.js
+++ models/Booking.js
@@ -1,25 +1,26 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const BookingSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-	lastName: {type: String, required: true},
-	email: {type: String, required: true},
-    location: {type: String, required: true},
-    gear: {type: Array, default: []},
-    upgrade: {
-        Referee : {type: Number},
-        Trainer: {type: Number}
-    },
-    intime: {
-        hours: {type: Number, required: true, min: 0, max: 23},
-        minutes: {type: Number, required: true, min: 0, max: 59}
-    },
-    outtime: {
-        hours: {type: Number, required: true, min: 0, max: 23},
-        minutes: {type: Number, required: true, min: 0, max: 59}
-    }
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  location: { type: String },
+  gear: { type: Array, default: [] },
+  upgrade: {
+    type: Array,
+    default: [],
+  },
+  intime: {
+    type: Number,
+    min: 0,
+    max: 23,
+  },
+  outtime: {
+    type: Number,
+    min: 0,
+    max: 23,
+  },
 });
 
-const Booking = mongoose.model('bookings', BookingSchema);
+const Booking = mongoose.model("bookings", BookingSchema);
 module.exports = Booking;
