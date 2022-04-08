c653206fd3acb6395d50e32a84cac99f93b48eb8
Jacob Day <day6@iu.edu>
2022-04-08 17:44:44 -0400

    added ShortcutModal component

---
+++ src/components/Dashboard/Shortcut/ShortcutModal/ShortcutModal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ShortcutModal extends Component {
+  state = {};
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default ShortcutModal;
