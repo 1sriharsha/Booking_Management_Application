2e2489ccad0700d506ee408c83193e2678ad7021
Jacob Day <day6@iu.edu>
2022-04-08 19:06:40 -0400

    updated InterestModal

--- src/components/Dashboard/Shortcut/ShortcutModal/InterestModal/InterestModal.jsx
+++ src/components/Dashboard/Shortcut/ShortcutModal/InterestModal/InterestModal.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { SupportedSports } from "../../../../../data";
 import styles from "./InterestModal.module.css";
 
 class InterestModal extends Component {
   state = {};
   render() {
+    const nInterests = SupportedSports.map(({ sportName }) => {
+      return (
+        <React.Fragment>
+          <section className={styles.interestBox}>
+            <input
+              className={styles.checkbox}
+              type={"checkbox"}
+              value={sportName}
+            />
+            <label className={styles.sportName} htmlFor={sportName}>
+              {sportName}
+            </label>
+          </section>
+        </React.Fragment>
+      );
+    });
+
     return (
       <React.Fragment>
         <div className={styles.modal}>
@@ -15,6 +33,20 @@ class InterestModal extends Component {
             >
               &times;
             </button>
+
+            <div className={styles.interestContainer}>
+              <section className={styles.title}>Select Your Interests</section>
+              <section className={styles.interests}>{nInterests}</section>
+
+              <section>
+                <button
+                  onClick={this.props.onCloseModal}
+                  className={[styles.button, styles.buttonPrimary].join(" ")}
+                >
+                  Submit
+                </button>
+              </section>
+            </div>
           </div>
         </div>
       </React.Fragment>
