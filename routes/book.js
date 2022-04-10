46991262d02517d55dbe88182d156df4ab5fa211
1sriharsha <48198559+1sriharsha@users.noreply.github.com>
2022-04-10 19:26:44 -0400

    api - 19:26, 04-10-2022

--- routes/book.js
+++ routes/book.js
@@ -37,29 +37,6 @@ router.get("/booked_slots",async function (req,res){
     })
 })
 
-// router.put("/:id", async(req,res)=>{
-//     if(req.body.bookId === req.params.id || req.user.isAdmin){
-//         if( req.body.password){
-//             try{
-//                 const salt = await bcrypt.genSalt(10);
-//                 req.body.password = await bcrypt.hash(req.body.password, salt);
-//             }catch(err){
-//                 return res.status(500).json(err);
-//             }
-//         }
-//         try{
-//             const user = await User.findByIdAndUpdate(req.params.id, {
-//                 $set: req.body,
-//             });
-//             res.status(200).json("Account has been updated");
-//         }catch(err){
-//             return res.status(500).json(err);
-//         }
-//     }else{
-//         return res.status(403).json("You can update only your account!");
-//     }
-// });
-
 router.put("/:id", async(req,res)=>{
     try{
         const modify = await Booking.findById(req.params.id);
