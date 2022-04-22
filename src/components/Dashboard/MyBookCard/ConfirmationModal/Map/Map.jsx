9c7e898a8ab9dc821cfdb1b78f3447d0ba0ec474
vaibhav201297 <vaibhav.vishwanath2097@gmail.com>
2022-04-22 01:55:43 -0400

    Trying to Add Directions

--- src/components/Dashboard/MyBookCard/ConfirmationModal/Map/Map.jsx
+++ src/components/Dashboard/MyBookCard/ConfirmationModal/Map/Map.jsx
@@ -4,7 +4,7 @@ import styles from "./Map.module.css";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
 const { REACT_APP_API_KEY } = process.env;
-
+var directions;
 const Map = ({ location, zoomLevel }) => (
   <div>
     <div className={styles.map}>
@@ -18,7 +18,13 @@ const Map = ({ location, zoomLevel }) => (
           lng={location.lng}
           text={location.address}
         />
+        
       </GoogleMapReact>
+        {/* <div >
+          
+          <h1>Coordinates</h1>
+          <button onClick={getLocation}>Get Location</button>
+        </div> */}
     </div>
   </div>
 );
@@ -29,4 +35,27 @@ const LocationPin = ({ text }) => (
   </div>
 );
 
+
+
+
+
+const origin = { lat: 40.756795, lng: -73.954298 };
+const destination = { lat: 41.756795, lng: -78.954298 };
+
+// directionsService.route(
+//   {
+//     origin: origin,
+//     destination: destination,
+//     travelMode: google.maps.TravelMode.DRIVING
+//   },
+//   (result, status) => {
+//     if (status === google.maps.DirectionsStatus.OK) {
+//       this.setState({
+//         directions: result
+//       });
+//     } else {
+//       console.error(`error fetching directions ${result}`);
+//     }
+//   }
+// );
 export default Map;
