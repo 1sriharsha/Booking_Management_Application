961b328f7d7a0279d44c23c697585082fb54727f
Vedant <vbenadik@iu.edu>
2022-04-23 22:07:25 -0400

    working chat support

--- src/components/Dashboard/Chat/Client/Client.js
+++ src/components/Dashboard/Chat/Client/Client.js
@@ -1,165 +1,74 @@
-import React, {Component} from 'react';
-import {Widget, addResponseMessage, addUserMessage, dropMessages} from 'react-chat-widget';
-import {CometChat} from '@cometchat-pro/chat';
+import { useEffect, useState } from "react";
+import MDSpinner from "react-md-spinner";
+// const appID = process.env.REACT_APP_ID;
+// const region = process.env.REACT_APP_REGION;
+// const AUTH_KEY = process.env.REACT_APP_AUTH_KEY;
+// const wid = process.env.REACT_APP_W1;
+const {REACT_APP_ID, REACT_APP_REGION, REACT_APP_AUTH_KEY, REACT_APP_W1} = process.env;
 
-import 'react-chat-widget/lib/styles.css';
-
-const { REACT_APP_APP_ID, REACT_APP_CHAT_API_KEY, REACT_APP_AGENT_ID, REACT_APP_LOCAL_URL, REACT_APP_PRODUCTION_URL } = process.env;
-
-var api_url;
-if (process.env.NODE_ENV === "production") {
-  api_url = REACT_APP_PRODUCTION_URL;
-} else {
-  api_url = REACT_APP_LOCAL_URL;
-}
-
-const agentUID = REACT_APP_AGENT_ID;
-const CUSTOMER_MESSAGE_LISTENER_KEY = "client-listener";
-const limit = 30;
-
-class Client extends Component {
-  componentDidMount() {
-    addResponseMessage('Welcome to our website! What can we help you with today?');
-    addResponseMessage('Let us know below and please wait for our customer support team to get back to you.');
-    
-    let uid = localStorage.getItem("cc-uid");
-    // check for uid, if exist then get auth token, login, create message listener and fetch previous messages
-   if ( uid !== null) {
-     this.fetchAuthToken(uid).then(
-       result => {
-         console.log('auth token fetched', result);
-         CometChat.login(result.authToken)
-         .then( user => {
-           console.log("Login successfully:", { user });
-           this.createMessageListener();
-           this.fetchPreviousMessages();
-           
-        })
-       },
-       error => {
-         console.log('Initialization failed with error:', error);
-       }
-     );
-   }
-  }
-
-  fetchAuthToken = async uid => {
-    const response = await fetch(api_url+`/chat/auth?uid=${uid}`)
-    const result = await response.json()
-    return result;
-  }
-
-  createUser = async () => {
-    const response = await fetch(api_url+`/chat/create`)
-    const result = await response.json()
-    return result;
-  }
-
-  createMessageListener = () => {
-    CometChat.addMessageListener(
-      CUSTOMER_MESSAGE_LISTENER_KEY,
-      new CometChat.MessageListener({
-        onTextMessageReceived: message => {
-          console.log("Incoming Message Log", { message });
-          addResponseMessage(message.text);
-        }
-      })
-    );
-  }
-
-  fetchPreviousMessages = () => {
-    var messagesRequest = new CometChat.MessagesRequestBuilder()
-    .setUID(agentUID)
-    .setLimit(limit)
-    .build();
-
-    messagesRequest.fetchPrevious().then(
-      messages => {
-        console.log("Message list fetched:", messages);
-        messages.forEach( message => {
-          if(message.receiver !== agentUID){
-            addResponseMessage(message.text);
-          } else {
-            addUserMessage(message.text)
-          }
+const Client = () => {
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+      console.log(REACT_APP_ID)
+    setLoad(true);
+    window.CometChatWidget.init({
+      appID: "2079391d99294330",
+      appRegion: 'us',
+      authKey: "7cf2425060750748696b231c31aec31d3ec06a17",
+    }).then((response) => {
+      console.log("Initialization completed successfully");
+      //You can now call login function.
+      let uid = localStorage.getItem("cc-uid");
+      if (uid === null) {
+        // create new user
+        const uid = "user" + new Date().getSeconds().toString();
+        const user = new window.CometChatWidget.CometChat.User(uid);
+        user.setName(uid);
+        window.CometChatWidget.createOrUpdateUser(user).then((user) => {
+          // Proceed with user login
+          window.CometChatWidget.login({
+            uid: uid,
+          }).then((loggedInUser) => {
+            localStorage.setItem("cc-uid", loggedInUser.uid);
+            // Proceed with launching your Chat Widget
+            window.CometChatWidget.launch({
+              widgetID: "5d3d5c17-15b9-4ed8-8077-a6124967615d",
+              roundedCorners: "true",
+              docked: "true",
+              height: "300px",
+              width: "400px",
+              defaultID: process.env.REACT_APP_AGENT_ID,
+              defaultType: "user", //user or group
+            });
+            setLoad(false);
+          });
         });
-      },
-      error => {
-        console.log("Message fetching failed with error:", error);
+      } else {
+        window.CometChatWidget.login({
+          uid: uid,
+        }).then((user) => {
+          window.CometChatWidget.launch({
+            widgetID: "5d3d5c17-15b9-4ed8-8077-a6124967615d",
+            roundedCorners: "true",
+            docked: "true",
+            height: "300px",
+            width: "400px",
+            defaultID: process.env.REACT_APP_AGENT_ID,
+            defaultType: "user", //user or group
+          });
+          setLoad(false);
+        });
+        
       }
-    );
-  }
-
-  handleNewUserMessage = newMessage => {
-    console.log(`New message incoming! ${newMessage}`);
-    var textMessage = new CometChat.TextMessage(
-      agentUID,
-      newMessage,
-      CometChat.MESSAGE_TYPE.TEXT,
-      CometChat.RECEIVER_TYPE.USER
-    );
-    let uid = localStorage.getItem("cc-uid");
-
-    if (uid === null) {
-      this.createUser().then(
-        result => {
-          console.log('auth token fetched', result);
-          localStorage.setItem("cc-uid",result.uid)
-          CometChat.login(result.authToken)
-          .then(user => {
-            console.log("Login successfully:", { user });
-            CometChat.sendMessage(textMessage).then(
-              message => {
-                console.log('Message sent successfully:', message);
-              },
-              error => {
-                console.log('Message sending failed with error:', error);
-              }
-            );
-            CometChat.addMessageListener(
-              CUSTOMER_MESSAGE_LISTENER_KEY,
-              new CometChat.MessageListener({
-                onTextMessageReceived: message => {
-                  console.log("Incoming Message Log", { message });
-                  addResponseMessage(message.text);
-                }
-              })
-            );
-          })
-      },
-      error => {
-        console.log('Initialization failed with error:', error);
-      })
-    } else {
-      // we have uid, do send
-      CometChat.sendMessage(textMessage).then(
-        message => {
-          console.log('Message sent successfully:', message);
-        },
-        error => {
-          console.log('Message sending failed with error:', error);
-        }
-      );
-    }
-  };
-
-  componentWillUnmount() {
-    CometChat.removeMessageListener(CUSTOMER_MESSAGE_LISTENER_KEY);
-    CometChat.logout();
-    dropMessages();
-  }
-
-  render() {
+    });
+  }, []);
+  if (load) {
     return (
-      <div className='App'>
-        <Widget
-          handleNewUserMessage={this.handleNewUserMessage}
-          title='Customer Support'
-          subtitle='We are ready to help you!'
-        />
+      <div className="container">
+        <MDSpinner />
       </div>
     );
   }
-}
-
+  return <div className="App"></div>;
+};
 export default Client;
