957331834c80ded9beac55798c511f7c4d2c6d92
vaibhav201297 <vaibhav.vishwanath2097@gmail.com>
2022-04-26 17:05:51 -0400

    Added Payment Details Update Method

--- routes/payment.js
+++ routes/payment.js
@@ -53,7 +53,7 @@ router.post("/add",async function(req,res){
 })
 
 router.post("/getpaymethod", async (req,res)=>{
-    console.log("Hello")
+   
     Payment.find({"userEmail":req.body.email}).then( (pMethods)=>{
         //console.log(pMethods)
         for(let method of pMethods){
@@ -63,6 +63,39 @@ router.post("/getpaymethod", async (req,res)=>{
     }).catch((err)=>{
         res.status(500).send(err)
     })
+})
+
+router.put("/update/:id",async (req,res)=>{
+    const salt = await bcrypt.genSalt(10)
+    try{
+        Payment.findByIdAndUpdate(req.params.id,{
+            userEmail:req.body.userEmail,
+            cardHolderName:req.body.cardHolderName,
+            cardNumber:req.body.cardNumber,
+            cvv:await bcrypt.hash(req.body.cvv,salt),
+            cardExpiry:req.body.cardExpiry,
+            billingLocation:{
+                streetAddress:req.body.billingLocation.streetAddress,
+                streetAddress2:req.body.billingLocation.streetAddress2 || "",
+                city:req.body.billingLocation.city,
+                state:req.body.billingLocation.state,
+                country:req.body.billingLocation.country,
+                zipcode:req.body.billingLocation.zipcode
+            },
+            promotionUsed:req.body.promotionUsed || "No Promotion Used",
+            rewardPointsUsed:req.body.rewardPointsUsed
+        }).then((response)=>{
+            console.log(response)
+            res.status(200).send("Payment Details Updated")
+        }).catch((err) =>{
+            console.log(err);
+            res.status(500).send("Internal Server Error")
+          });
+    }
+    catch(err){
+        console.log(err)
+    }
+
 })
 
 module.exports = router;
